c3c690106a6abaf049898b32e23474816368840b
oto-ml <rmunoz1803@alumno.ipn.mx>
2022-04-20 17:03:49 -0500

    Prueba para mission commander

--- test/missionCommander.test.js
+++ test/missionCommander.test.js
@@ -1,7 +1,9 @@
-describe("Esto es una suite de pruebas", () => {
-    test('Caso de prueba 1', () => {
-      const result = 1 + 2 
-      expect(result).toBe(3);
+const MissionCommander = require('./../app/missionCommander');
+
+describe("Unit test for mission commander class", () => {
+    test('1) Create a mission commander object', () => {
+      const myMissionCommander = new MissionCommander("Woopa") 
+      expect(myMissionCommander.name).toBe("Woopa");
     });
   })
   
